0b1f5b213cf681687a929a790bfdd56eabd207ce
albertost5 <sancheztorresalberto@gmail.com>
2024-01-15 19:43:49 +0100

    Add error handling to searchCapital method

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,7 +1,7 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Country} from '../interfaces/country';
-import {Observable} from 'rxjs';
+import {catchError, Observable, of} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -14,6 +14,10 @@ export class CountriesService {
   }
 
   searchCapital(capitalName: string): Observable<Country[]> {
-    return this.http.get<Country[]>(`${this.basePath}/capital/${capitalName}`);
+    const url = `${this.basePath}/capital/${capitalName}`;
+    
+    return this.http.get<Country[]>(url).pipe(
+      catchError(err => of([]))
+    );
   }
 }
